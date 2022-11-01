7330ab2aa0468a47f7841db7494cb50e17284ce8
yongs21 <yongs21@douzone.com>
2022-11-01 13:27:35 +0900

    git init

---
+++ src/test/java/me/shiteship/inflearnthejavatest/InflearnTheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.shiteship.inflearnthejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnTheJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
